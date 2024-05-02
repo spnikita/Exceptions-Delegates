8efd9ad305b91f2ebf5cdd9df50d6950a47b4691
nikita <nikita.yazkov@yandex.ru>
2024-05-02 11:29:13 +0300

    Задание 1. Массив из 5 различных видов исключений, включая собственный тип исключения.

---
+++ Task1/CustomException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task1
+{
+    /// <summary>
+    /// Кастомное исключение
+    /// </summary>
+    internal class CustomException : Exception
+    {
+        public CustomException()
+        { }
+
+        public CustomException(string message) : base(message)
+        { }
+    }
+}
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Task1
 {
@@ -6,7 +7,28 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var exceptions = new Exception[]
+            {
+                new FormatException("Значение не находится в соответствующем формате для преобразования из строки методом преобразования, например Parse ."),
+                new FileNotFoundException("Файл не существует."),
+                new DriveNotFoundException("Диск недоступен или не существует."),
+                new DirectoryNotFoundException("Недопустимая часть пути к каталогу."),
+                new CustomException("Кастомнеое исключение.")
+            };
+            
+            foreach (var exception in exceptions)
+            {
+                try
+                {
+                    throw exception;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
